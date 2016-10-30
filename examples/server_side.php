7e71a9da2fa498939c40146e6492a0350a0511d1
Moe <github@m03.ca>
2016-10-29 23:52:30 -0700

    fixes

--- examples/server_side.php
+++ examples/server_side.php
@@ -1,30 +1,28 @@
 <?php
-//set return type as JSON string
+////set return type as JSON string
 header('Content-Type: application/json');
-//set default return variable
+////set default return variable
 $json='{"ok":false}';
-//check if data is setecho json_encode($_POST);exit();
-if(isset($_POST['data'])&&$_POST['data']!==''){
+////check if data is setecho json_encode($_POST);exit();
     
-//XXXXXXXXXXXXXXXXXXXXX must be replaced with your api key. This is generated in your superuser settings.
+////XXXXXXXXXXXXXXXXXXXXX must be replaced with your api key. This is generated in your superuser settings.
     
-//Penguin Server
+////Penguin Server
 //    $url = "http://free.api.unmetered.chat/XXXXXXXXXXXXXXXXXXXXX/data";
-//Husky Server
+////Husky Server
 //    $url = "http://api.unmetered.chat/XXXXXXXXXXXXXXXXXXXXX/data";
-    $url = "http://free.api.unmetered.chat/XXXXXXXXXXXXXXXXXXXXX/".$_GET['cmd'];
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POST,count($_POST));
-    curl_setopt($curl, CURLOPT_POSTFIELDS,$_POST);
-    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,10000); 
-    curl_setopt($curl, CURLOPT_TIMEOUT, 400);
-    $json = curl_exec($curl);
+$url = "http://free.api.unmetered.chat/XXXXXXXXXXXXXXXXXXXXX/".$_GET['cmd'];
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST,count($_POST));
+curl_setopt($curl, CURLOPT_POSTFIELDS,$_POST);
+curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,10000); 
+curl_setopt($curl, CURLOPT_TIMEOUT, 400);
+$json = curl_exec($curl);
 
-    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-    curl_close($curl);
-}
+curl_close($curl);
 echo $json;
 ?>
